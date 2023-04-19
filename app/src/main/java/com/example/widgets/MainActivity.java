36e658e5c4ce9c71df7b119cee02ea365c14a344
a22karja <a22karja@student.his.se>
2023-04-19 16:35:22 +0200

    Added listener to Button so it sais "YOU PRESSED ME"

--- app/src/main/java/com/example/widgets/MainActivity.java
+++ app/src/main/java/com/example/widgets/MainActivity.java
@@ -1,14 +1,28 @@
 package com.example.widgets;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        android.widget.Button button2=findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "YOU PRESSED ME", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
